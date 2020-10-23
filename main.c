0ad03f8f4baed232d22617ab6c89d86f32f19936
mayhathaway <mayhath@gmail.com>
2020-10-23 15:57:03 -0400

    added makefile

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 
 int main() {
 
+  printf("problem one:\n");
   int i;
   int total = 0;
   int repeats = 0;
@@ -22,5 +23,21 @@ int main() {
   int result = total - repeats;
   printf("result: %d\n", result);
 
+
+  printf("problem six:\n");
+  int sum_individual = 0;
+  int sum_total = 0;
+
+  for (i = 0; i <= 100; i++) {
+    sum_individual += i * i;
+  }
+
+  for (i = 0; i <= 100; i++) {
+    sum_total += i;
+  }
+  sum_total = sum_total * sum_total;
+
+  result = sum_total - sum_individual;
+  printf("result: %d\n", result);
   return 0;
 }
